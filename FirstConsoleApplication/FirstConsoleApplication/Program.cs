4d98db2d34d2139a5246355b16b32fb0cec8ced3
Erica <Erica@ERICA-TOSH>
2017-05-16 22:08:53 -0300

    Added print commands

--- FirstConsoleApplication/FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/FirstConsoleApplication/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace FirstConsoleApplication
 {
+    /// <summary>
+    /// A class that prints messages
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method to print message
+        /// </summary>
         static void Main(string[] args)
         {
+            // Bloom's Taxonmy Level
+            // print a message
             Console.WriteLine("Hello World");
+            // print blank line
+            Console.WriteLine();
         }
     }
 }
